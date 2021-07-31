f85efab8ae90cc59ace79197315379d415a90e92
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-07-31 18:28:42 +0530

    connect with mysql

---
+++ src/lk/ijse/dep7/sms_lite/TM/StudentTM.java
@@ -0,0 +1,38 @@
+package lk.ijse.dep7.sms_lite.TM;
+
+public class StudentTM {
+    private String id;
+    private String name;
+
+    public StudentTM() {
+    }
+
+    public StudentTM(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentTM{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
+++ src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
@@ -1,21 +1,66 @@
 package lk.ijse.dep7.sms_lite.controller;
 
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+import lk.ijse.dep7.sms_lite.TM.StudentTM;
+
+import java.sql.*;
 
 public class MainFormController {
     public TextField txtID;
     public TextField txtName;
     public TextField txtPhone;
-    public ListView lstContact;
-    public TableView tblStudents;
+    public ListView<String> lstContact;
+    public TableView<StudentTM> tblStudents;
+    Connection connection;
+
+    public void initialize(){
+        tblStudents.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblStudents.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<StudentTM, Button> lastCol = (TableColumn<StudentTM, Button>) tblStudents.getColumns().get(2);
+        lastCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StudentTM, Button>, ObservableValue<Button>>() {
+            @Override
+            public ObservableValue<Button> call(TableColumn.CellDataFeatures<StudentTM, Button> param) {
+                Button delete = new Button("Delete");
+                return (ObservableValue<Button>) delete;
+            }
+        });
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sms_lite", "root", "mysql");
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM student");
+            while (rst.next()){
+                String id = rst.getString("id");
+                String name = rst.getString("name");
+                tblStudents.getItems().add(new StudentTM(id, name));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                if (!connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
 
     public void txtPhone_OnAction(ActionEvent actionEvent) {
     }
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
+        String id = txtID.getText();
+        String name = txtName.getText();
+        String phone = txtPhone.getText();
     }
 
     public void btnClear_OnAction(ActionEvent actionEvent) {
